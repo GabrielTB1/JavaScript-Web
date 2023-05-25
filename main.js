e7607b74b759688adc079e928fb252b1c1b1be04
Gabriel Terres Bednascki <gabterres@hotmail.com>
2023-05-25 16:02:33 -0300

    Utilizando For

--- main.js
+++ main.js
@@ -4,9 +4,7 @@ function tocaSom (idElementoAudio) {
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
-
-while(contador < listaDeTeclas.length){
+for(var contador = 0; contador < listaDeTeclas.length; contador++){
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
@@ -15,5 +13,4 @@ while(contador < listaDeTeclas.length){
     tecla.onclick = function () {
         tocaSom(idAudio);
     };
-    contador++;
 }
